62c7e524986eaf728f40b995b5cb63d83c17384d
Juanes1103 <je.diazc12@uniandes.edu.co>
2023-03-28 11:29:15 -0500

    Esqueleto Final

--- Proyecto 1/src/Consola/consola.java
+++ Proyecto 1/src/Consola/consola.java
@@ -41,9 +41,9 @@ public class consola {
 	
 	
 	public void menu() {
-		System.out.println("1. A");
-		System.out.println("2. B");
-		System.out.println("3. C");
+		System.out.println("1. Menu Administrador");
+		System.out.println("2. Menu Empleado");
+		System.out.println("3. Menu Huesped");
 		System.out.println("0. Salir");
 	}
 	
@@ -51,9 +51,60 @@ public class consola {
 		
 		if (opcion == 1) {
 			
+			menuAdministrador();
+			
+			Scanner seleccion = new Scanner(System.in);
+			int sel = seleccion.nextInt();
+			
+			if(sel == 1) {
+				
+				System.out.println("Opción A");
+				
+			} else if (sel == 2) {
+				
+			} else if (sel == 0) {
+				System.out.println("Saliendo...");
+			} else {
+				System.out.println("No se ha ingresado una opción válida!");
+			}
+			
 		} else if (opcion == 2) {
 			
+			menuEmpleado();
+			
+			Scanner seleccion = new Scanner(System.in);
+			int sel = seleccion.nextInt();
+			
+			if(sel == 1) {
+				
+				System.out.println("Opción A");
+				
+			} else if (sel == 2) {
+				
+			} else if (sel == 0) {
+				System.out.println("Saliendo...");
+			} else {
+				System.out.println("No se ha ingresado una opción válida!");
+			}
+			
 		} else if (opcion == 3) {
+			
+			menuHuesped();
+			
+			Scanner seleccion = new Scanner(System.in);
+			int sel = seleccion.nextInt();
+			
+			if(sel == 1) {
+				
+				System.out.println("Opción A");
+				
+			} else if (sel == 2) {
+				
+			} else if (sel == 0) {
+				System.out.println("Saliendo...");
+			} else {
+				System.out.println("No se ha ingresado una opción válida!");
+			}
 			
 		} else if (opcion == 0) {
 			System.out.println("Saliendo...");
@@ -62,5 +113,26 @@ public class consola {
 			System.out.println("No se ha seleccionado una opcion válida!");
 		}
 		
+	}
+	
+	public void menuAdministrador() {
+		System.out.println("\nMenu Administrador");
+		System.out.println("1) Crear Tarifa");
+		System.out.println("2) Crear Habitación");
+		System.out.println("0) Salir");
+	}
+	
+	public void menuEmpleado() {
+		System.out.println("\nMenu Empleado");
+		System.out.println("1) Crear Reserva");
+		System.out.println("2) Cargar Pago");
+		System.out.println("0) Salir");
+	}
+	
+	public void menuHuesped() {
+		System.out.println("\nMenu Huespes");
+		System.out.println("1) Ingresar Datos");
+		System.out.println("2) Consultar Reserva");
+		System.out.println("3) Salir");
 	}
 }
